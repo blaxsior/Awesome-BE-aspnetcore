c3dd93df7e300a9e821f39d9846a1dcd4c857744
blaxsior <heejun2798@gmail.com>
2024-09-27 16:32:13 +0900

    docs: aspnet DI lifetime에 대한 주석 내용 수정

--- src/common/config/InjectConfig.cs
+++ src/common/config/InjectConfig.cs
@@ -2,9 +2,9 @@ using common.jwt;
 
 namespace common.config.inject {
   public class InjectConfig {
-    // Singleton: 프로젝트 생명 주기에서 단 하나만 생성됨
-    // Transcient: 요청할 때마다 생성
-    // Scoped: 주입 위치마다 하나씩 생성. 주입 위치가 다르면 다른 객체
+    // Singleton: 프로젝트 생명 주기에서 단 하나만 생성하고 모든 요청에서 재사용
+    // Scoped: 인스턴스를 하나의 요청에 대해 만들고 재사용
+    // Transcient: 인스턴스가 하나의 요청 내에서도 매번 새롭게 생성
     public static void Config(WebApplicationBuilder builder) {
       builder.Services.AddSingleton<IJwtService, JwtService>();
     }
